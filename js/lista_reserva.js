e3aace4c6abba6c566636422652cb3d748e351a2
Diego Espil <diegoespil92@gmail.com>
2024-04-15 16:03:16 -0300

    fix varios

--- js/lista_reserva.js
+++ js/lista_reserva.js
@@ -12,13 +12,11 @@ console.log(tabla)
 let nuevaFila = tabla.insertRow()
 
 // Insertar celdas en la nueva fila
-let celdaId = nuevaFila.insertCell(0)
-let celdaServicio = nuevaFila.insertCell(1)
-let celdaFecha = nuevaFila.insertCell(2)
-let celdaHora = nuevaFila.insertCell(3)
+let celdaServicio = nuevaFila.insertCell(0)
+let celdaFecha = nuevaFila.insertCell(1)
+let celdaHora = nuevaFila.insertCell(2)
 
 // Agregar contenido a las celdas (puedes obtener estos valores de un formulario, por ejemplo)
-celdaId.innerHTML = id_servicio
 celdaServicio.innerHTML = servicio;
 celdaFecha.innerHTML = fecha;
 celdaHora.innerHTML = hora
